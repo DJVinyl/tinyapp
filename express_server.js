129b05f8f9942b5fa5c17586b01e34b3ca68ac9a
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-15 21:45:47 +0000

    added RandomString function

--- express_server.js
+++ express_server.js
@@ -3,12 +3,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+//located code: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+const generateRandomString = () => {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 7; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 };
 
 app.set("view engine", "ejs"); //setting the view engine.
@@ -24,6 +34,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {

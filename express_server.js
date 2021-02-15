fc68f745fdad41af99ad7c44d046c5f44d4bed0d
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-15 20:53:17 +0000

    added /urls and /urls ejs sheet

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //setting the view engine.
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,6 +15,11 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {

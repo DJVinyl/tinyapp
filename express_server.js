ce9631673084fa425491ac825a25ab4f301915c4
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-15 22:00:58 +0000

    added redirection for shortURLs

--- express_server.js
+++ express_server.js
@@ -38,7 +38,7 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  const shortURL = generateRandomString()
+  const shortURL = generateRandomString() //EVENTUALLY ADD FUNCTIONALITY THAT WILL CHECK IF The shortURL already exists,
   urlDatabase[shortURL] = req.body.longURL;
   console.log(shortURL, req.body.longURL)
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
@@ -50,8 +50,14 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {

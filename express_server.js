3473bfb44a51e2ee174de4f7a4b26719ede9f682
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-16 23:01:18 +0000

    change header to ahve a login button and register button. As well add GET for login page, Login POST to still be implemented

--- express_server.js
+++ express_server.js
@@ -11,14 +11,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-const users = {};
+const users = {
+  '10000': {
+    id: "10000", 
+    email: "test", 
+    password: "123"
+  }
+};
 
 //located code: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-const generateRandomString = () => {
+const generateRandomString = (length) => {
   let result           = '';
   const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
-  for (let i = 0; i < 7; i++ ) {
+  for (let i = 0; i < length; i++ ) {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
@@ -35,15 +41,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase };
+  console.log(req.cookies['user_id']);
+  const templateVars = {
+    userID: req.cookies['user_id'],
+    urls: urlDatabase
+  }
   res.render("urls_index", templateVars);
 });
 
+
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  const shortURL = generateRandomString() //EVENTUALLY ADD FUNCTIONALITY THAT WILL CHECK IF The shortURL already exists,
+  const shortURL = generateRandomString(7) //EVENTUALLY ADD FUNCTIONALITY THAT WILL CHECK IF The shortURL already exists,
   urlDatabase[shortURL] = req.body.longURL;
   console.log(shortURL, req.body.longURL)
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
@@ -52,12 +61,14 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"] };
+    userID: req.cookies['user_id'] };
+  // const templateVars = { 
+  //   username: req.cookies["username"] }
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], userID: req.cookies["user_id"] };
   res.render("urls_show", templateVars);
 });
 
@@ -84,26 +95,42 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
+  //res.cookie('username', req.body.username);
+  res.cookie('user_id', req.body.userID)
   res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.body.username);
+  res.clearCookie('user_id', req.body.userID);
   res.redirect('/urls');
 });
 
 app.get("/register", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"] };
+    userID: req.cookies['user_id'] };
   res.render("register", templateVars);
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = { 
+    userID: req.cookies['user_id'] };
+  res.render("login", templateVars);
+});
+
 app.post("/register", (req, res) => {
-  users[req.body.username] = req.body.password;
-  res.cookie('username', req.body.username);
+  const randID = generateRandomString(10);
+  users[randID] = { id: randID.toString(), username: req.body.username, password: req.body.password};
+  res.cookie('user_id', randID);
+  //res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
+
+app.post("/login", (req, res) => {
+  console.log("I entered the login page POST")
+  //res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
 
 
 app.listen(PORT, () => {

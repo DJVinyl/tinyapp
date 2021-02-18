1f26a0cbb8e22ec23d15e47c65dcac24e5190398
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-18 21:29:42 +0000

    Linted and commented

--- express_server.js
+++ express_server.js
@@ -1,163 +1,114 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-var cookieSession = require('cookie-session')
-const bcrypt = require('bcrypt');
-//const {getUserByEmail} = require('./helpers');
 
+//Requirements
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+
+const bcrypt = require("bcryptjs"); // using bcryptJS
+// bcrypt 5.0.0 is installed and works but due to errors mentioned in compass and
+//by recommendation, bcryptjs is what is being used.
+
+//Helper functions
+const { generateRandomString } = require("./helpers");
+const { checkEmailExists } = require("./helpers");
+const { loginAuth } = require("./helpers");
+const { userAuthorized } = require("./helpers");
+const { urlsForUser } = require("./helpers");
+const { checkURL } = require("./helpers");
 
 //
 ///// Middleware
 //
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
-  name: 'session',
-  // Cookie Options
-  keys: ['key1'],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    // Cookie Options
+    keys: ["key1"],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 //
-///// Const Variable used for testing purposes.
+///// Database objects
 //
+const urlDatabase = {};
+const users = {};
 
-const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: '10000' },
-  "9sm5xK": { longURL: "http://www.google.com", userID: '10000'}
-};
+//What they look like and for testing purposes:
+// const urlDatabase = {
+//   'b2xVn2': { longURL: "http://www.lighthouselabs.ca", userID: test },
+//   '9sm5xK': { longURL: "http://www.google.com", userID: test },
+// };
+//const user = {'tester': { email: 'test', password: '123' }};
 
-const users = {
-  '10000': {
-    id: "10000", 
-    email: "test", 
-    password: "123"
-  }
-};
+//setting the view engine.
+app.set("view engine", "ejs");
 
-//
-///// Helper Functions
-//
-
-//located code: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-const generateRandomString = (length) => {
-  let result           = '';
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
-
-const checkEmailExists = (newEmail) => {
-  for(let id in users){
-    if (users[id].email === newEmail){
-      return true;
-    }
-  }
-  return false;
-};
-
-const loginAuth = (username, password) => {
-  for (let id in users) {
-    if (users[id].email === username){
-      if(bcrypt.compareSync(password, users[id].password)){
-        return id;
-      }
-    }
-  }
-  return false;
-}
-
-const userAuthorized = (user, shortURL) => {
-  const accessibleURLS = urlsForUser(user);
-  for (let urlID in accessibleURLS)
-  {
-    if (shortURL === urlID){
-      return true;
-    }
-  }
-  return false;
-};
-
-const urlsForUser = (id) => {
-  let result = {};
-  for (let urlID in urlDatabase){
-    if (urlDatabase[urlID].userID === id) {
-      result[urlID] = urlDatabase[urlID];
-    }
-  }
-  return result;
-};
-
-const checkURL = (id) => {
-  for (let urlID in urlDatabase){
-    if (urlID === id) {
-      return true;
-    }
-  }
-  return false;
-};
-
-app.set("view engine", "ejs"); //setting the view engine.
-
+//Default
 app.get("/", (req, res) => {
-  if(req.session.user_id){
-    res.redirect('/urls');
+  if (req.session.user_id) {
+    res.redirect("/urls");
   } else {
-    res.redirect('/login');
+    res.redirect("/login");
   }
 });
 
 //
 ///// GET and POST functions
 //
-
 app.get("/urls", (req, res) => {
   const templateVars = {
     userID: req.session.user_id,
-    urls: urlsForUser(req.session.user_id),
+    urls: urlsForUser(req.session.user_id, urlDatabase),
     user: users[req.session.user_id],
   };
   if (!req.session.user_id) {
-    res.redirect('/login');
+    res.redirect("/login");
   }
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString(7) 
-  //ADD FUNCTIONALITY THAT WILL CHECK IF The shortURL already exists,
-  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session.user_id}
-  res.redirect(`/urls/${shortURL}`)
+  const shortURL = generateRandomString(7);
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id,
+  };
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
-    res.redirect('/login');
+    res.redirect("/login");
   } else {
-  const templateVars = { 
-    userID: req.session.user_id,
-    user: users[req.session.user_id]
-   };
-  res.render("urls_new", templateVars);
+    const templateVars = {
+      userID: req.session.user_id,
+      user: users[req.session.user_id],
+    };
+    res.render("urls_new", templateVars);
   }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
     userID: req.session.user_id,
-    user: users[req.session.user_id]
+    user: users[req.session.user_id],
   };
 
-  const test = checkURL(req.params.shortURL);
-  const authorized = userAuthorized(req.session.user_id, req.params.shortURL)
+  const test = checkURL(req.params.shortURL, urlDatabase);
 
-  if(test && authorized) {
+  const authorized = userAuthorized(
+    req.session.user_id,
+    req.params.shortURL,
+    urlDatabase
+  );
+
+  if (test && authorized) {
     res.render("urls_show", templateVars);
   } else {
     res.status(404).send({ error: "Resource not located or Unauthorized" });
@@ -168,24 +119,22 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   let newURL = req.body.editSubmit;
   urlDatabase[req.params.shortURL].longURL = newURL;
-  res.redirect(`/urls/${req.params.shortURL}`);
+  res.redirect(`/urls/`);
 });
 
 //Redirect the to long URL
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL].longURL;
-  if (!longURL.includes('http'))
-  {
-    longURL = 'http://' + longURL;
+  if (!longURL.includes("http")) {
+    longURL = "http://" + longURL;
   }
   res.redirect(longURL);
 });
 
-
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls')
+  res.redirect("/urls");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -193,8 +142,8 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  req.session = null
-  res.redirect('/urls');
+  req.session = null;
+  res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
@@ -222,36 +171,42 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const randID = generateRandomString(10);
-  if(!req.body.username || !req.body.password)
-  {
+  const randID = generateRandomString(10); // 10 character rand string
+  if (!req.body.username || !req.body.password) {
     res.status(400).send({ error: "Username and Email must be values" });
-  } else if (checkEmailExists(req.body.username)){
+  } else if (checkEmailExists(req.body.username, users)) {
     res.status(400).send({ error: "Username already exists" });
   } else {
-  users[randID] = { id: randID.toString(), email: req.body.username, password: bcrypt.hashSync(req.body.password, 10)};
-  req.session.user_id = randID;
-  res.redirect('/urls');
+    users[randID] = {
+      id: randID.toString(),
+      email: req.body.username,
+      password: bcrypt.hashSync(req.body.password, 10),
+    };
+    req.session.user_id = randID;
+    res.redirect("/urls");
   }
 });
 
 app.post("/login", (req, res) => {
   const loginEmail = req.body.email;
   const loginPW = req.body.password;
-  const ID = loginAuth(loginEmail,loginPW)
-  if(ID)
-  {
+  const ID = loginAuth(loginEmail, loginPW, users);
+  if (ID) {
     req.session.user_id = ID;
     //res.cookie('user_id', ID);
-    res.redirect('/urls');
-  }
-  else {
-    res.status(403).send({ error: "Login Failed. Username and password combo don't exist. Or incorrect password." });
+    res.redirect("/urls");
+  } else {
+    res.status(403).send({
+      error:
+        "Login Failed. Username and password combo don't exist. Or incorrect password.",
+    });
   }
 });
 
-app.get('*', (req,res) => {
-  res.status(404).send({ error: "HEY, THIS PART OF THE WEBSITE DOESN'T EXIST YET!" });
+app.get("*", (req, res) => {
+  res
+    .status(404)
+    .send({ error: "HEY, THIS PART OF THE WEBSITE DOESN'T EXIST YET!" });
 });
 
 app.listen(PORT, () => {
--- helpers.js
+++ helpers.js
@@ -1,11 +1,101 @@
+const bcrypt = require('bcryptjs');
+
+//located code: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+//
+//// Generates a random string which takes an argument of length and returns a random string of that length
+//
+const generateRandomString = (length) => {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+//
+//// checks to see if someone is already registered with the email.
+//
+const checkEmailExists = (newEmail, usersDB) => {
+  for (let id in usersDB) {
+    if (usersDB[id].email === newEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//
+//// Login Authenticator which checks username and password against DB
+//
+const loginAuth = (username, password, userDB) => {
+  for (let id in userDB) {
+    if (userDB[id].email === username) {
+      if (bcrypt.compareSync(password, userDB[id].password)) {
+        return id;
+      }
+    }
+  }
+  return false;
+};
+
+//
+//// Checks to see if a user is authorized to be in accessing a shortURL. ShortURL may exist but may not be registered to the user. THis function retrns a boolean if the user does pertain to the shortURL.
+//
+const userAuthorized = (user, shortURL, urlDatabase) => {
+  const accessibleURLS = urlsForUser(user,urlDatabase);
+  for (let urlID in accessibleURLS) {
+    if (shortURL === urlID) {
+      return true;
+    }
+  }
+  return false;
+};
+//
+//// Returns an fraction database from the urlDatabase that pertains to the user's ID
+//
+const urlsForUser = (id, urlDatabase) => {
+  let result = {};
+  for (let urlID in urlDatabase) {
+    if (urlDatabase[urlID].userID === id) {
+      result[urlID] = urlDatabase[urlID];
+    }
+  }
+  return result;
+};
+///
+//// Checks to see if the shortID is indeed in the urlDatabase.
+//
+const checkURL = (id, urlDatabase) => {
+  for (let urlID in urlDatabase) {
+    if (urlID === id) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//
+/// Function given by compass. Used for mocha and Chai testing
+//
 const getUserByEmail = function(email, database) {
   let user = undefined;
-  for(let id in database){
-    if (database[id].email === email){
+  for (let id in database) {
+    if (database[id].email === email) {
       user = database[id].id;
     }
   }
   return user;
 };
 
-module.exports = {getUserByEmail};
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  checkEmailExists,
+  loginAuth,
+  userAuthorized,
+  urlsForUser,
+  checkURL
+};
